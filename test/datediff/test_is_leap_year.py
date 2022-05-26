abac514b8bee67c021b4ce886fbde7b13b62d5ee
James Constable <jwdconstable@gmail.com>
2022-05-26 22:46:49 +1000

    Break test classes into multiple files

---
+++ test/datediff/test_is_leap_year.py
@@ -0,0 +1,34 @@
+import unittest
+
+import datediff
+
+class TestIsLeapYear(unittest.TestCase):
+    '''
+    Unit tests for is_leap_year function.
+    '''
+
+    def test_regular_non_leap_year(self):
+        '''
+        Test case for an obvious non-leap year (not divisible by 4).
+        '''
+        self.assertFalse(datediff.is_leap_year(2001))
+
+    def test_regular_leap_year(self):
+        '''
+        Test case for an obvious leap year (divisible by 4, not a century).
+        '''
+        self.assertTrue(datediff.is_leap_year(2004))
+
+    def test_non_leap_century(self):
+        '''
+        Test case for a century year that is not a leap year under the
+        Gregorian calendar.
+        '''
+        self.assertFalse(datediff.is_leap_year(1900))
+
+    def test_leap_century(self):
+        '''
+        Test case for a century year that is a leap year under the Gregorian
+        calendar.
+        '''
+        self.assertTrue(datediff.is_leap_year(2000))
